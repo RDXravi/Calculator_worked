9a28023ae5369e5ef524a684cb393f19d3983283
RDXravi <pirateravidixit@gmail.com>
2017-02-17 01:27:07 +0530

    first

--- app/src/main/java/com/example/ravi/calculator/divide.java
+++ app/src/main/java/com/example/ravi/calculator/divide.java
@@ -4,4 +4,12 @@ package com.example.ravi.calculator;
  * Created by rAVI on 8/21/16.
  */
 public class divide {
+
+    public static void tt(){
+
+        System.out.println("ffllll");
+
+
+    }
+
 }
